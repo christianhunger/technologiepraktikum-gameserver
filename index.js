48a7d840b2eae0bf07de861129f15c3e27ba51c5
christianhunger <christian.hunger@exxcellent.de>
2019-01-14 09:18:52 +0100

    chore(*): initial commit

---
+++ index.js
@@ -0,0 +1,112 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const EloRating = require("elo-rating");
+const KittenOpponents = require("./kitten-testdata");
+
+
+const opponents = KittenOpponents;
+
+const app = express();
+app.use(bodyParser.json());
+
+/**
+ * {winnerId: *, loserId: *}
+ */
+app.post('/update-rating', (req, res) => {
+    const { winnerId, loserId } = req.body;
+
+    const winner = opponents[winnerId];
+    const loser = opponents[loserId];
+
+    const { playerRating, opponentRating } = EloRating.calculate(
+        winner.rating,
+        loser.rating,
+        true
+    );
+
+    winner.rating = playerRating;
+    opponentRating.rating = opponentRating;
+
+    res.send(JSON.stringify(opponents));
+});
+
+app.get('/opponents', (req, res) => {
+    res.send(JSON.stringify(opponents));
+});
+
+app.get('/next-round', (req, res) => {
+    const { firstId, secondId } = twoDifferentRandomIdsInRange(1, 10);
+
+    const nextRound = {
+        opponent1: {
+            id: firstId,
+            imageUrl: opponents[firstId].imageUrl
+        },
+        opponent2: {
+            id: secondId,
+            imageUrl: opponents[secondId].imageUrl
+        }
+    };
+
+    res.send(JSON.stringify(nextRound));
+});
+
+app.get('/leaderboard', (req, res) => {
+    res.send(JSON.stringify(opponentsSortedByRating(opponents)));
+});
+
+app.listen(3000, () => {
+    console.log('Game server listening on port 3000!');
+});
+
+/**
+ * Geenrates a random integer id in the range [min, max].
+ * Throws an error if max <= min!
+ *
+ * @param {number} min - The lower bound.
+ * @param {number} max - The upper bound.
+ * @returns {number}
+ */
+const randomIntInRange = (min, max) => {
+    if (max <= min) {
+        throw "Invalid arguments: max <= min.";
+    }
+
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    const result = Math.floor(Math.random() * (max - min)) + min;
+    return result;
+};
+
+/**
+ * Generates two distinct integer ids in the range [min, max].
+ * Throws an error if max <= min!
+ *
+ * @param {number} min - The lower bound.
+ * @param {number} max - The upper bound.
+ * @returns {{firstId: *, secondId: *}} - A tuple containing the ids.
+ */
+const twoDifferentRandomIdsInRange = (min, max) => {
+    if (max <= min) {
+        throw "Invalid arguments: max <= min.";
+    }
+
+    const firstId = randomIntInRange(min, max);
+    let secondId = randomIntInRange(min, max);
+
+    while (firstId === secondId) {
+        secondId = randomIntInRange(min, max);
+    }
+
+    return { firstId, secondId };
+};
+
+const opponentsSortedByRating = opponents => Object.entries(opponents)
+    .map(entry => {
+        return {
+            id: entry[0],
+            ...entry[1]
+        };
+    })
+    .sort((opponent1, opponent2) => opponent2.rating - opponent1.rating);
+
---
+++ kitten-testdata.js
@@ -0,0 +1,55 @@
+const kittens = {
+  1: {
+    name: "Tiger",
+    rating: 1000,
+    imageUrl: "http://127.0.0.1:8080/kittens/kitten1.jpg"
+  },
+  2: {
+    name: "Max",
+    rating: 1000,
+    imageUrl: "http://127.0.0.1:8080/kittens/kitten2.jpg"
+  },
+  3: {
+    name: "Smokey",
+    rating: 1000,
+    imageUrl: "http://127.0.0.1:8080/kittens/kitten3.jpg"
+  },
+  4: {
+    name: "Sam",
+    rating: 1000,
+    imageUrl: "http://127.0.0.1:8080/kittens/kitten4.jpg"
+  },
+  5: {
+    name: "Kitty",
+    rating: 1000,
+    imageUrl: "http://127.0.0.1:8080/kittens/kitten5.jpg"
+  },
+  6: {
+    name: "Sassy",
+    rating: 1000,
+    imageUrl: "http://127.0.0.1:8080/kittens/kitten6.jpg"
+  },
+  7: {
+    name: "Shadow",
+    rating: 1000,
+    imageUrl: "http://127.0.0.1:8080/kittens/kitten7.jpg"
+  },
+  8: {
+    name: "Simba",
+    rating: 1000,
+    imageUrl: "http://127.0.0.1:8080/kittens/kitten8.jpg"
+  },
+  9: {
+    name: "Misty",
+    rating: 1000,
+    imageUrl: "http://127.0.0.1:8080/kittens/kitten9.jpg"
+  },
+  10: {
+    name: "Lucky",
+    rating: 1000,
+    imageUrl: "http://127.0.0.1:8080/kittens/kitten10.jpg"
+  }
+};
+
+
+module.exports = kittens;
